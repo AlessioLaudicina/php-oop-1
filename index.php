3c83ec420dcaffb4ce9f7ee2c5a2d02cd6ce6136
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-05-04 15:50:15 +0200

    aggiunto botstrap e vue tramite cdn

---
+++ index.php
@@ -0,0 +1,24 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!-- BOOTSTRAP -->
+     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous' />
+
+     <!-- CSS -->
+     <link rel="stylesheet" href="css/style.css">
+
+      <!-- VUE -->
+      <script src="https://unpkg.com/vue@3"></script>
+      <title>Movies</title>
+</head>
+<body>
+    <h1>prova</h1>
+    
+</body>
+</html>
